16172f829972b2abc285a94baa09e06891359fac
JamesSmith1202 <jsmith@stuy.edu>
2017-12-06 14:37:16 -0500

    stole from euclid lmao

--- funky_town.js
+++ funky_town.js
@@ -18,6 +18,17 @@ let randomStudent = function() {
 	return STUDENTS[ parseInt(Math.random() * STUDENTS.length) ]
 }
 
+var gcd = function(a, b){
+    if(a<b){
+	return gcd(b,a);
+    }
+    var r = a % b;
+    if (r == 0){
+	return b;
+    }
+    return gcd(b, r);
+}
+
 /*
 console.log(fibonacci(1));
 console.log(fibonacci(2));
